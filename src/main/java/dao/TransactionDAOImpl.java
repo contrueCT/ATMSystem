568c5337f0e4f5a78d4b352ca1e7fb34b2c80fbe
con <contrue_ct@outlook.com>
2025-02-16 20:31:48 +0800

    feat:完成了Transaction和UserDAO的实现类

---
+++ src/main/java/dao/TransactionDAOImpl.java
@@ -0,0 +1,63 @@
+package dao;
+
+import model.Transaction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.List;
+
+public class TransactionDAOImpl implements TransactionDAO {
+    @Override
+    public boolean addTransaction(Transaction transaction) {
+        if(transaction == null){
+            return false;
+        }
+        String sql = "insert into transactions (user_id, type, amount, source_card, target_card) value (?,?,?,?,?)";
+        try(Connection conn = DruidDBConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+            pstmt.setInt(1, transaction.getUser_id());
+            pstmt.setString(2, transaction.getType());
+            pstmt.setBigDecimal(3, transaction.getAmount());
+            pstmt.setString(4, transaction.getSourceCard());
+            pstmt.setString(5, transaction.getTargetCard());
+            pstmt.executeUpdate();
+            return true;
+
+        }catch (SQLException e){
+            System.out.println("添加交易记录失败"+e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public List<Transaction> findTransactionByCardID(String card_id) {
+        String sql = "select * from transactions where source_card = ?";
+        List<Transaction> transactions = null;
+
+        try(Connection conn = DruidDBConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+            pstmt.setString(1, card_id);
+            ResultSet rs = pstmt.executeQuery();
+            Transaction transaction = null;
+            while(rs.next()){
+                transaction = new Transaction();
+                transaction.setId(rs.getInt("id"));
+                transaction.setUser_id(rs.getInt("user_id"));
+                transaction.setType(rs.getString("type"));
+                transaction.setAmount(rs.getBigDecimal("amount"));
+                transaction.setSourceCard(rs.getString("source_card"));
+                transaction.setTargetCard(rs.getString("target_card"));
+                transactions.add(transaction);
+            }
+            return transactions;
+        }catch (SQLException e){
+            System.out.println("获取全部交易记录失败"+e.getMessage());
+        }
+        return Collections.emptyList();
+    }
+}
---
+++ src/main/java/dao/UserDAOImpl.java
@@ -0,0 +1,103 @@
+package dao;
+
+import model.User;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public boolean addUser(User user) {
+        if(user==null){
+            System.out.println("用户不能为空");
+            return false;
+        }
+        String sql = "insert into user (name, phone, id_card, card_number, password) values(?,?,?,?,?)";
+
+        try(Connection conn = DruidDBConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, user.getName());
+            pstmt.setString(2, user.getPhone());
+            pstmt.setString(3,user.getId_card());
+            pstmt.setString(4, user.getCard_number());
+            pstmt.setString(5, user.getPassword());
+            int rows = pstmt.executeUpdate();
+            return rows>0;
+        } catch (SQLException e) {
+            System.out.println("添加用户失败:"+ e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateBalance(String card_id, BigDecimal balance) {
+        String sql = "update user set balance = ? where id_card = ?";
+        try(Connection conn = DruidDBConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+            pstmt.setBigDecimal(1, balance);
+            pstmt.setString(2, card_id);
+            pstmt.executeUpdate();
+            return true;
+        }catch (SQLException e){
+            System.out.println("添加用户失败:"+ e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public User findUserById(String card_id) {
+        String sql = "select * from user where id_card = ?";
+        User user = null;
+        try(Connection conn = DruidDBConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+            pstmt.setString(1, card_id);
+            ResultSet rs = pstmt.executeQuery();
+            user.setId(rs.getInt("id_card"));
+            user.setId_card(rs.getString("id_card"));
+            user.setName(rs.getString("name"));
+            user.setPhone(rs.getString("phone"));
+            user.setCard_number(rs.getString("card_number"));
+            user.setPassword(rs.getString("password"));
+            user.setBalance(rs.getBigDecimal("balance"));
+        }catch(SQLException e){
+            System.out.println("查找用户失败:"+ e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        String sql = "select * from user";
+        List<User> users = null;
+        try(Connection conn = DruidDBConnection.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+
+            ResultSet rs = pstmt.executeQuery();
+            User user = null;
+            while(rs.next()){
+                user = new User();
+                user.setId(rs.getInt("id_card"));
+                user.setId_card(rs.getString("id_card"));
+                user.setName(rs.getString("name"));
+                user.setPhone(rs.getString("phone"));
+                user.setCard_number(rs.getString("card_number"));
+                user.setPassword(rs.getString("password"));
+                user.setBalance(rs.getBigDecimal("balance"));
+                users.add(user);
+            }
+            return users;
+        }catch (SQLException e){
+            System.out.println("获取全部用户信息失败:"+ e.getMessage());
+        }
+        return Collections.emptyList();
+    }
+}
